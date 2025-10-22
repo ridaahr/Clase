b8d898190c36f6f092856822be86ed6ece0e8449
ridaahr <raharrar12@gmail.com>
2025-10-22 09:53:51 +0200

    clase

--- despliegueServidor/repaso.php
+++ despliegueServidor/repaso.php
@@ -4,6 +4,7 @@ include "funcionesrepaso.php";
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -18,6 +19,7 @@ include "funcionesrepaso.php";
         }
     </style>
 </head>
+
 <body>
     <?php
     $prueba = multiplos(19, -100);
@@ -43,17 +45,19 @@ include "funcionesrepaso.php";
     for ($i = 0; $i < 10; $i++) {
         echo "<tr>";
         for ($j = 0; $j < 10; $j++) {
-            if ($i == 4 && $j == 4) {
+            if (($i == 4 || $i == 5) && ($j == 4 || $j == 5)) {
                 echo "<td class=\"center\">" . $bid[$i][$j] . "</td>";
             } else if ($i == $j || $i + $j == 9) {
                 echo "<td class=\"red\">" . $bid[$i][$j] . "</td>";
             } else {
-                echo "<td>". $bid[$i][$j] . "</td>";
+                echo "<td>" . $bid[$i][$j] . "</td>";
             }
         }
         echo "</tr>";
     }
     echo "</table>";
+
     ?>
 </body>
+
 </html>
