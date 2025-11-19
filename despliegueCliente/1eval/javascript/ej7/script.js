0109a7065d4a867f23d388a817a5241a75605112
ridaahr <raharrar12@gmail.com>
2025-11-19 14:25:10 +0100

    as

--- despliegueCliente/1eval/javascript/ej7/script.js
+++ despliegueCliente/1eval/javascript/ej7/script.js
@@ -21,3 +21,49 @@ console.log(persona);
 delete persona.ciudad;
 console.log(persona);
 
+
+persona.saludar = function() {
+    alert("Hola, soy " + persona.nombre);
+}
+
+persona.cumplirAnos = function() {
+    persona.edad += 1;
+}
+
+console.log(persona);
+delete persona.ciudad;
+console.log(persona);
+
+const estudiante1 = {
+    nombre: "eustaquio",
+    nota: 5
+}
+
+const estudiante2 = {
+    nombre: "josefina",
+    nota: 7
+}
+
+const estudiante3 = {
+    nombre: "sandalio",
+    nota: 8
+}
+
+const estudiantes = [
+    estudiante1,
+    estudiante2,
+    estudiante3
+];
+
+estudiantes.media = function() {
+    let sum = 0;
+    for (let i = 0; i < estudiantes.length; i++) {
+        sum += estudiantes[i]["media"];
+    }
+
+    return sum / estudiantes.length;
+};
+
+let media = estudiantes.media();
+alert(media);
+
