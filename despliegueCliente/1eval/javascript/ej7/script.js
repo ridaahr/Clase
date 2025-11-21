aca9f372e524cd6f76dc1cfc041d32bbf016e728
diurno <diurno@B27-D3.lan.tierno.es>
2025-11-21 08:48:42 +0100

    as

--- despliegueCliente/1eval/javascript/ej7/script.js
+++ despliegueCliente/1eval/javascript/ej7/script.js
@@ -58,12 +58,54 @@ const estudiantes = [
 estudiantes.media = function() {
     let sum = 0;
     for (let i = 0; i < estudiantes.length; i++) {
-        sum += estudiantes[i]["media"];
+        sum += estudiantes[i]["nota"];
     }
 
     return sum / estudiantes.length;
 };
 
+estudiantes.aprobados = function() {
+    let aprobados = 0;
+    for (let i = 0; i < estudiantes.length; i++) {
+        if(estudiantes[i]["nota"] > 5) {
+            aprobados++;
+        } 
+    }
+
+    return aprobados;
+};
+
 let media = estudiantes.media();
 alert(media);
 
+let aprobados = estudiantes.aprobados();
+alert(aprobados);
+
+estudiantes.buscarPorNombre = function(nombre) {
+    let found = false;
+    for (let i = 0; i < estudiantes.length; i++) {
+        if(estudiantes[i]["nombre"] == nombre) {
+            return estudiantes[i];
+            found = true;
+        } 
+    }
+    if (!found) {
+        return "No se ha encontrado el alumno";
+    }
+}
+
+let buscar = estudiantes.buscarPorNombre("asd");
+alert(buscar);
+
+estudiantes.actualizarNota = function(nombre, nuevaNota) {
+    let found = false;
+    for (let i = 0; i < estudiantes.length; i++) {
+        if(estudiantes[i]["nombre"] == nombre) {
+            estudiantes[i]["nota"] == nuevaNota
+            found = true;
+        } 
+    }
+    if (!found) {
+        return "No se ha encontrado el alumno";
+    }
+}
