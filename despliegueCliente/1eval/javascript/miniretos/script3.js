cccbea5d7f94c5156f68d49f637c4fb1ee2b5ba3
diurno <diurno@B27-B4.lan.tierno.es>
2025-12-12 09:31:13 +0100

    Mensaje por defecto

---
+++ despliegueCliente/1eval/javascript/miniretos/script3.js
@@ -0,0 +1,9 @@
+let h2 = document.querySelectorAll(".titulin");
+let pdiv = document.querySelectorAll("div.contenido p");
+let activovisible = document.querySelectorAll(".activo.visible");
+let importantedestacar = document.querySelectorAll(".importante, .destacar");
+
+console.log(h2.length);
+console.log(pdiv.length);
+console.log(activovisible.length);
+console.log(importantedestacar.length);
